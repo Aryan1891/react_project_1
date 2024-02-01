cc447ca29cc99fe24d902a11a84175b954e80851
Aryan1891 <aryanjangirofficial@gmail.com>
2024-02-01 10:55:30 +0530

    LECTURE 7 created routes for different pages like about us , contact us and error.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import "./index.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => (
   <>
@@ -13,7 +16,25 @@ const AppLayout = () => (
     <Footer />
   </>
 );
+const appRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<AppLayout/>,
+    errorElement:<Error/>,
+  },
+  {
+    path:"/about",
+    element:<About />,
+  },
+  {
+    path:"/contact",
+    element:<Contact />,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<AppLayout />);
+
+
+root.render(<RouterProvider router={appRouter} />);
+ 
