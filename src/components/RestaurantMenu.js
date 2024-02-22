41c4f2a66f9ea219a1b7124c5090a39625b4a45a
Aryan1891 <aryanjangirofficial@gmail.com>
2024-02-22 10:40:12 +0530

    lecture 9 made custom hook for restaurant menu items

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,20 +1,9 @@
-import { useState,useEffect } from "react"
-import { MENU_LINK } from "../utils/constants"
 import { useParams } from "react-router-dom"
+import useRestaurantMenu from "../utils/useRestaurantMenu"; 
 const RestaurantMenu=()=>{
-  const[restaurantItems,setRestaurantItems ]=useState([])
-  const {resId}=useParams();
-
-  useEffect(()=>{
-  fetchMenu()
-  },[])
-
-  const fetchMenu=async()=>{
-    const data=await fetch(MENU_LINK + resId)
-    const json= await data.json()
-    console.log(json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.categories[0].itemCards)
-    setRestaurantItems(json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.categories[0].itemCards)
-  }
+  
+  const {resId} = useParams();
+  const restaurantItems=useRestaurantMenu(resId);
 
   return(
     <div className="menuItems">
---
+++ src/utils/useRestaurantMenu.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { MENU_LINK } from "../utils/constants"
+
+const useRestaurantMenu =(resId)=>{
+    const [restaurantItems, setRestaurantItems]=useState([])
+
+  useEffect(()=>{
+  fetchMenu()
+  },[])
+ 
+    const fetchMenu=async()=>{
+    const data=await fetch(MENU_LINK + resId)
+    const json= await data.json()
+    console.log(json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards)
+    setRestaurantItems(json.data.cards[2].groupedCard.cardGroupMap.REGULAR.cards[2].card.card.itemCards)
+  }
+
+  return restaurantItems
+
+
+}
+
+export default useRestaurantMenu
+
